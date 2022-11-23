0e2775a14492942cc5f7a6f1e49bd8a8d7655075
MacheteBy <Macheteby@gmail.com>
2022-11-23 12:24:43 +0300

    test

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function checkObj(obj, checkProp) {
     if(obj.hasOwnProperty(checkProp) === true){
-      return checkProp;
+      return obj[checkProp];
     } else{
       return "Not Found";
     }
     
   }
+
+  console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift"));
