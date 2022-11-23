d02004e32786e843230140ebf70d2d02c0f501d6
MacheteBy <Macheteby@gmail.com>
2022-11-23 13:26:28 +0300

    decision

--- script.js
+++ script.js
@@ -171,3 +171,19 @@ for(let i = 0; i < myArr.length; i++){
 }
 
 console.log(total);
+
+//freecodecamp js task
+
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for(let i = 0; i < arr.length; i++){
+        for(let k = 0; k < arr[i].length; i++){
+            product += arr[i][k];
+        }
+    }
+    // Only change code above this line
+    return product;
+  }
+  
+  console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
