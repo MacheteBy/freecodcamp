ffd67b2f86ec3b1fd5e9649dbf0a506e4bec46d0
MacheteBy <Macheteby@gmail.com>
2022-11-23 13:26:33 +0300

    decision

--- script.js
+++ script.js
@@ -178,12 +178,16 @@ function multiplyAll(arr) {
     let product = 1;
     // Only change code below this line
     for(let i = 0; i < arr.length; i++){
-        for(let k = 0; k < arr[i].length; i++){
-            product += arr[i][k];
+        for(let k = 0; k < arr[i].length; k++){
+            product *= arr[i][k];
         }
     }
     // Only change code above this line
     return product;
   }
   
-  console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
+  console.log(multiplyAll([[5, 1], [0.2, 4, 0.5], [3, 9]]));
+
+  //freecodecamp js task
+
+  
