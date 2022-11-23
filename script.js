4579c7125a43bd1676e7543ac4a064d5e949e344
MacheteBy <Macheteby@gmail.com>
2022-11-23 12:31:33 +0300

    decision

--- script.js
+++ script.js
@@ -13,4 +13,31 @@ function checkObj(obj, checkProp) {
 
   //freecodecamp js task
 
-  
+  const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+        "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ]
+    }
+  ];
+
+    //freecodecamp js task
+
+
+    
