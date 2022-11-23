51b01230f8d74bb4a2e240b2c33a84b6a5a0d4fe
MacheteBy <Macheteby@gmail.com>
2022-11-23 13:17:58 +0300

    decision

--- script.js
+++ script.js
@@ -160,3 +160,14 @@ for(let i = 9; i >= 1; i-=2){
     myArray.push(i);
 }
 console.log(myArray);
+
+//freecodecamp js task
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+
+for(let i = 0; i < myArr.length; i++){
+    total +=myArr[i];
+}
+
+console.log(total);
