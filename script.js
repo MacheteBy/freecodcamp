57ed4da753d988e92dc1cbee7bc8df46c54dcfe0
MacheteBy <Macheteby@gmail.com>
2022-11-24 11:14:59 +0300

    first

--- script.js
+++ script.js
@@ -348,3 +348,14 @@ function rangeOfNumbers(startNum, endNum) {
 //freecodecamp js task
 
 
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i = 'function scope';
+}
+
+console.log(checkScope());
