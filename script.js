49ebc6fa176984df81751f2b915f851435743bfb
MacheteBy <Macheteby@gmail.com>
2022-11-26 11:05:46 +0300

    first

--- script.js
+++ script.js
@@ -558,3 +558,9 @@ let reRegex = /^(\d+) \1 \1$/; // Change this line
 let result = reRegex.test(repeatNum);
 
 //freecodecamp js task
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
+
+//freecodecamp js task
