e95cd583b00612de3fcd7d46f478721623e414e9
MacheteBy <Macheteby@gmail.com>
2022-11-26 11:09:30 +0300

    first

--- script.js
+++ script.js
@@ -585,3 +585,14 @@ console.log(output);
 console.clear();
 
 //freecodecamp js task
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+
+
+console.log(typeof(seven));
+console.log(typeof(three));
+
+
+//freecodecamp js task
