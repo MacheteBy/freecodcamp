b4e98aa09b6bdfd89b7c5ecf39224f55e85e62c3
MacheteBy <Macheteby@gmail.com>
2022-11-26 11:17:09 +0300

    first

--- script.js
+++ script.js
@@ -636,3 +636,13 @@ let result = getNine();
 console.log(result);
 
 //freecodecamp js task
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
+
+//freecodecamp js task
