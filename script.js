8cc60ef7df10013514a4fc6305d386d9bf5c8b2c
MacheteBy <Macheteby@gmail.com>
2022-11-26 11:00:50 +0300

    first

--- script.js
+++ script.js
@@ -510,3 +510,6 @@ let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; // Change this line
 let result = userCheck.test(username);
 
 //freecodecamp js task
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
