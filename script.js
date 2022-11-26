ce6b3173f105dc960f65e50489a1278413f91815
MacheteBy <Macheteby@gmail.com>
2022-11-26 10:51:18 +0300

    first

--- script.js
+++ script.js
@@ -442,3 +442,6 @@ let result = vowelRegex; // Change this line
 
 //freecodecamp js task
 
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
