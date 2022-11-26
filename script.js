9dc1e05ad091c9da6596f4403d8ef795b138c314
MacheteBy <Macheteby@gmail.com>
2022-11-26 10:53:02 +0300

    first

--- script.js
+++ script.js
@@ -450,3 +450,10 @@ let result = quoteSample.match(alphabetRegex); // Change this line
 let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
+
+//freecodecamp js task
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou^0-9]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+//freecodecamp js task
