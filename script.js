c23086135981b3a2a9c2586929ebcd912d6203df
MacheteBy <Macheteby@gmail.com>
2022-11-26 10:49:59 +0300

    first

--- script.js
+++ script.js
@@ -435,3 +435,10 @@ let unRegex = /.un/; // Change this line
 let result = unRegex.test(exampleStr);
 
 //freecodecamp js task
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = vowelRegex; // Change this line
+
+//freecodecamp js task
+
