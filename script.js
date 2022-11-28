6fc174ecb3ec6cedf48ea3f7b479a6958463c0a3
MacheteBy <Macheteby@gmail.com>
2022-11-28 10:53:21 +0300

    first

--- script.js
+++ script.js
@@ -692,3 +692,9 @@ function myFunc() {
 
 let yourArray = ["a", "b", "c", true, false, 5, 6]; // Change this line
 
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 7;
+// Only change code above this line
+console.log(myArray);
+
