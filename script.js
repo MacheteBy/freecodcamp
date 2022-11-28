1c1c9c9c292d2d731a80185f7b1cfa80ebaa9bda
MacheteBy <Macheteby@gmail.com>
2022-11-28 11:10:44 +0300

    first

--- script.js
+++ script.js
@@ -741,3 +741,11 @@ console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'Pal
 
 
 
+function forecast(arr) {
+  // Only change code below this line
+arr = arr.slice(2, 4);
+  return arr;
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
